39f9e25d5f54916ba6dc84c152b797743fabd518
Oleg <oleg.freidkin@gmail.com>
2021-11-28 17:33:49 +0300

    Базовая программа

--- SF.U3/Program.cs
+++ SF.U3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		Console.Write("What is your name? ");
+		var name = Console.ReadLine();
+		Console.Write("What is your age? ");
+		var age = checked((byte)int.Parse(Console.ReadLine()));
+		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+		Console.Write("What is your favorite day of week? ");
+
+		var day = (DayOfWeek)int.Parse(Console.ReadLine());
+		Console.WriteLine("Your favorite day is {0}", day);
+
+	}
+}
