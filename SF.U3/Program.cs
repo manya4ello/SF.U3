4fc2aef9fe44d517e65e5d8d13391e808cc4804b
Oleg <oleg.freidkin@gmail.com>
2021-11-28 18:28:42 +0300

    Ввод ДР

--- SF.U3/Program.cs
+++ SF.U3/Program.cs
@@ -12,14 +12,28 @@ class MainClass
 
 		Console.Write("What is your name? ");
 		var name = Console.ReadLine();
+
 		Console.Write("What is your age? ");
 		var age = checked((byte)int.Parse(Console.ReadLine()));
-		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
+		Console.Write("What's your birthday date? ");
+		DateTime birthday = Convert.ToDateTime(Console.ReadLine());
+		DateTime today = DateTime.Now;
+		var howold = today.Subtract(birthday);
+		var howoldyear = today.Year - birthday.Year; 
+		Console.WriteLine("Your name is {0} and age is {1} ( {2} г.р.) ", name, age, birthday.ToLongDateString());
+		Console.WriteLine("However, according to my calculations you're {0} year old.", howoldyear);
+		
 		Console.Write("What is your favorite day of week? ");
 
 		var day = (DayOfWeek)int.Parse(Console.ReadLine());
+		if (day < 0) 
+				{
+			Console.WriteLine("Your favorite day is {0}", day);
+		}
+		else Console.WriteLine("It seems like you have no favorite day of week");
 		Console.WriteLine("Your favorite day is {0}", day);
-
+		Console.WriteLine("By the way, {0} was {1}", birthday, birthday.DayOfWeek);
+		Console.ReadKey();
 	}
 }
